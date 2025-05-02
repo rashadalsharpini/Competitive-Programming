ed118a33dac2875be6adc2b51a6b8959d145b9ea
rashad <rashadalsharpini@gmail.com>
2025-05-03 02:12:25 +0300

    upd

--- stuff/topologicalSort.cpp
+++ stuff/topologicalSort.cpp
@@ -26,51 +26,47 @@ const int dx[]{0, 1, 0, -1, -1, -1, 1, 1};
 const int dy[]{1, 0, -1, 0, -1, 1, -1, 1};
 
 template <typename T> istream &operator>>(istream &input, vector<T> &data) {
-    for (T &x : data) input >> x;
-    return input;
+  for (T &x : data) input >> x;
+  return input;
 }
 template <typename T> ostream &operator<<(ostream &output, const vector<T> &data) {
-    for (const T &x : data) output << x << ' ';
-    return output;
+  for (const T &x : data) output << x << ' ';
+  return output;
 }
 const int MOD = 1e9+7;
 
 void solve() {
     int nodes, edges;cin>>nodes>>edges;
     vector<vi> adj(nodes+1);
-    vector<bool> vis(nodes+1, false);
-    int u, v;
+    vector<int> vis(nodes+1, false);
     for (int i = 0; i < edges; i++) {
-        cin>>u>>v;
+        int u, v;cin>>u>>v;
         adj[u].push_back(v);
-        adj[v].push_back(u);
     }
+    vi topo;
+    bool notCyclic=1;
     function<void(int)> dfs = [&](int p) {
-        vis[p] = 1;
-        cout<<p<<" ";
+        vis[p] = 2;
         for (auto v : adj[p]) {
             if (!vis[v]) dfs(v);
+            else if(vis[v]==2)notCyclic=0;
         }
+        vis[p]=1;
+        topo.push_back(p);
     };
-    int count=0;
     for (int i = 1; i <= nodes; i++) {
-        if(!vis[i]){
-            count++;
-            dfs(i);
-            cout<<endl;
-        }
+        if(!vis[i])dfs(i);
     }
-    debug(count);
-    cout<<(edges==nodes-count?"tree":"not tree")<<endl;
+    if(notCyclic)cout<<topo<<endl;
+    else cout<<-1<<endl;
 }
-
 int32_t main() {
-    /*freopen("whereami.in", "r", stdin);*/
-    /*freopen("whereami.out", "w", stdout);*/
-    fastio
-    int t = 1;
-    /*cin>>t;*/
-    while (t--)
-        solve();
-    return 0;
+  /*freopen("whereami.in", "r", stdin);*/
+  /*freopen("whereami.out", "w", stdout);*/
+  fastio
+  int t = 1;
+  /*cin>>t;*/
+  while (t--)
+    solve();
+  return 0;
 }
